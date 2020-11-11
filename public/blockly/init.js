bf43a13142cd4436aede3a077b2dfbf9ddca55db
PSlyp <nonny_038@hotmail.com>
2020-11-11 23:52:39 +0700

    add feature resize

--- public/blockly/init.js
+++ public/blockly/init.js
@@ -1,4 +1,7 @@
-var workspace = Blockly.inject('blocklyDiv', {
+var blocklyArea = document.getElementById('blocklyArea')
+var blocklyDiv = document.getElementById('blocklyDiv')
+
+var workspace = Blockly.inject(blocklyDiv, {
     toolbox: document.getElementById('toolbox'),
     grid: {
         spacing: 20,
@@ -17,3 +20,22 @@ var workspace = Blockly.inject('blocklyDiv', {
     },
     trashcan: true
 })
+
+var onresize = function(e) {  
+    var element = blocklyArea
+    var x = 0
+    var y = 0
+    do {
+      x += element.offsetLeft
+      y += element.offsetTop
+      element = element.offsetParent
+    } while (element)
+    blocklyDiv.style.left = x + 'px'
+    blocklyDiv.style.top = y + 'px'
+    blocklyDiv.style.width = blocklyArea.offsetWidth + 'px'
+    blocklyDiv.style.height = blocklyArea.offsetHeight + 'px'
+    Blockly.svgResize(workspace)
+  };
+  window.addEventListener('resize', onresize, false)
+  onresize()
+  Blockly.svgResize(workspace)
